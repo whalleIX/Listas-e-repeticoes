f25d449e8c00088995677d797f492ca992b31579
unknown <jhuanandrade4@gmail.com>
2023-04-06 14:33:57 -0300

    Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes

---
+++ notasMedia.py
@@ -0,0 +1,9 @@
+notas = [3, 6, 8, 10]
+media = 0.0
+alunos = ['joãozinho', 'pedrinho', 'carlinhos']
+
+print(notas[0])
+
+for valorIndex in notas:
+    for nome in alunos:
+        print(valorIndex + nome)    
